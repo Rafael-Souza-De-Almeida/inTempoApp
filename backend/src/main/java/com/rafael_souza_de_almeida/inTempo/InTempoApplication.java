40eeac72f6d4e7d83d1ae8b990ce5543ae739927
rafael-almeida <rafaelalmeida37@ufrrj.br>
2025-02-04 02:07:37 -0300

    First commit

---
+++ backend/src/main/java/com/rafael_souza_de_almeida/inTempo/InTempoApplication.java
@@ -0,0 +1,13 @@
+package com.rafael_souza_de_almeida.inTempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InTempoApplication.class, args);
+	}
+
+}
