afb46fe26cf9effb389f1e594176e7b848c96701
rafael-almeida <rafaelalmeida37@ufrrj.br>
2025-02-28 01:30:51 -0300

    Melhorando responsividade na tela de login e sign_up

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/login/LoginForm";
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-12">
-      <div className="w-[450px]">
+      <div className="w-[350px] md:w-[450px]">
         <LoginForm />
       </div>
     </div>
--- frontend/src/app/sign_up/page.tsx
+++ frontend/src/app/sign_up/page.tsx
@@ -3,7 +3,7 @@ import { SignUpForm } from "@/components/sign_up/signUpForm";
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-12">
-      <div className="w-[450px]">
+      <div className=" w-[350px] md:w-[450px]">
         <SignUpForm />
       </div>
     </div>
